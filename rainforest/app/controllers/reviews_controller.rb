91652235a22ff771812c9246d915b60f6bc3737d
emanuelsamel <mannys94@hotmail.com>
2017-06-19 19:07:33 -0400

    final rainforest2

--- rainforest/app/controllers/reviews_controller.rb
+++ rainforest/app/controllers/reviews_controller.rb
@@ -1,18 +1,43 @@
 class ReviewsController < ApplicationController
 
   def create
+      @product = Product.find(params[:product_id])
+      @review = @product.reviews.new(review_params)
 
-  end
+      if @review.save
+        flash[:notice] = "Your review has been successfully created!"
+        redirect_to products_path
+      else
+        render "product"
 
-  def edit
+      end
+    end
 
-  end
+    def edit
+      @review = Review.find(params[:id])
+      @product = Product.find(params[:product_id])
+    end
 
-  def update
+    def update
+      @product = Product.find(params[:product_id])
+      @review = Review.find(params[:id])
+      if @review.update(review_params)
+        flash[:notice] = "Your review has been successfully updated!"
+        redirect_to product_path(@product)
+      else
 
-  end
+        render :edit
+      end
+    end
 
-  def destroy
+    def destroy
+      @review = Review.find(params[:id])
+      @review.destroy
+      flash[:notice] = "Your review has been successfully DESTROYED!"
+      redirect_to root_path
+    end
 
-  end
+    def review_params
+      params.require(:review).permit(:comment)
+    end
 end
