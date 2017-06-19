66e176bf7dbd593baa65942d42bc5be8709603b2
mikesmall <michael.l.a.small@gmail.com>
2017-06-19 13:20:12 -0400

    ProductsController added with 7 'empty' methods

---
+++ rainforest/app/controllers/products_controller.rb
@@ -0,0 +1,24 @@
+class ProductsController < ApplicationController
+
+  def index
+  end#index
+
+  def create
+  end#create
+
+  def new
+  end#new
+
+  def edit
+  end#edit
+
+  def show
+  end#show
+
+  def update
+  end#update
+
+  def destroy
+  end#destroy
+
+end#ProductsController
