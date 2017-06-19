007e57122d5c3b054834fd234d00f0e8c838216f
emanuelsamel <mannys94@hotmail.com>
2017-06-19 15:04:19 -0400

    created product

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -1,14 +1,29 @@
 class ProductsController < ApplicationController
 
   def index
-    @product = Product.all
+    @products = Product.all
   end
 
   def create
-  end#create
+
+    @product = Product.create product_params
+
+    if @product.save
+      # default behaviour of flash works on a redirect
+      flash[:notice] = 'Product successfully created!'
+      redirect_to root_url
+    else
+      # flash.now works on the same request
+      flash.now[:error] = 'Sorry, try again!'
+      render :new
+    end
+
+
+  end
 
   def new
-  end#new
+    @product = Product.new
+  end
 
   def edit
   end#edit
@@ -21,5 +36,12 @@ class ProductsController < ApplicationController
 
   def destroy
   end#destroy
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+
+  end
 
 end#ProductsController
