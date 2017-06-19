f02e4e269434f8e74d39eb60a2119af85d4f9ba2
mikesmall <michael.l.a.small@gmail.com>
2017-06-19 13:32:07 -0400

    Database seeded via seeds.rb

--- rainforest/db/seeds.rb
+++ rainforest/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tire_iron = Product.create(name: 'Tire Iron', description: 'Just a tire iron', price_in_cents: 999)
+
+black_hat = Product.create(name: 'Black Hat', description: 'This goes on your head', price_in_cents: 34999)
+
+popcorn_maker = Product.create(name: 'Popcorn Maker', description: 'Never leave your house again', price_in_cents: 999999)
